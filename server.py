a5d3dfb3cd7eb321c68a5f997d1fdb6a2d2072ab
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-04 16:03:45 +0200

    Added mongo and JWT

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from flask import Flask
-from pymongo import MongoClient
+from flask_jwt_extended import JWTManager
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-client = MongoClient()
 
+#Initialize mongo client
+with open("mongourl.secret") as url_file:
+    app.config["MONGO_URI"] = url_file.read()[:-1]
+    mongo = PyMongo(app)
+
+#Initialize JWT
+with open("jwt.secret") as key_file:
+    app.config["JWT_SECRET_KEY"] = key_file.read()[:-1]
+    jwt = JWTManager(app)
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", 7777)
+    app.run("0.0.0.0", 7777, True)
