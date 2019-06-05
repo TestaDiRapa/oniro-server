28829dd0eba6143e84f6f4357dc73f7d77a69176
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-05 10:29:24 +0200

    Refactored methods

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_jwt_extended import JWTManager, create_access_token
 from flask_pymongo import PyMongo
-from flask_restful import Api, Resource
 from passlib.hash import pbkdf2_sha256 as sha256
 from utils import error_message
 
 app = Flask(__name__)
-api = Api(app)
 
 # Initialize mongo client
 with open("/root/oniro-server/mongourl.secret") as url_file:
@@ -27,128 +25,114 @@ def add_claims_to_access_token(data):
     }
 
 
-class UserLogin(Resource):
+@app.route('/login/<string:user_type>', methods=['GET'])
+def login(user_type):
+    if request.method != 'GET':
+        return make_response("error", 405)
 
-    def get(self):
-        params = request.get_json(silent=True)
-        if params is None:
-            return error_message("wrong parameters")
+    if user_type != "user" and user_type != "doctor":
+        return make_response("error", 404)
 
-        if "cf" not in params or "password" not in params:
-            return error_message("username and password are mandatory fields!")
+    params = request.get_json(silent=True)
+    if params is None:
+        return error_message("wrong parameters")
 
-        try:
-            user = mongo.db.users.find_one({"_id": params["cf"]})
+    if "cf" not in params or "password" not in params:
+        return error_message("username and password are mandatory fields!")
 
-            if user is None:
-                return error_message("user doesn't exists!")
+    try:
+        user = mongo.db[user_type+"s"].find_one({"_id": params["cf"]})
 
-            pwd = user["password"]
-            if not sha256.verify(params["password"], pwd):
-                return error_message("password is not correct")
+        if user is None:
+            return error_message("user doesn't exists!")
 
-            access_token = create_access_token({"username": params["cf"], "type": "user"})
-            return jsonify(status="ok", access_token=access_token)
+        pwd = user["password"]
+        if not sha256.verify(params["password"], pwd):
+            return error_message("password is not correct")
 
-        except:
-            return error_message("error in database connection!")
+        access_token = create_access_token({"username": params["cf"], "type": "user"})
+        return jsonify(status="ok", access_token=access_token)
 
-
-class UserRegister(Resource):
-
-    def put(self):
-        json_data = request.get_json(silent=True, cache=False)
-        if json_data is None:
-            return error_message("mime type not accepted")
-
-        fields = ["cf", "email", "password"]
-
-        for field in fields:
-            if field not in json_data:
-                return error_message(field + " is a mandatory field")
-
-        try:
-            if mongo.db.users.find_one({'_id': json_data["cf"]}) is not None:
-                return error_message("a user with this CF already exists!")
-
-            mongo.db.users.insert_one(
-                {
-                    '_id': json_data['cf'],
-                    'email': json_data['email'],
-                    'password': sha256.hash(json_data["password"])
-                }
-            )
-
-            access_token = create_access_token({"username": json_data["cf"], "type": "user"})
-            return jsonify(status="ok", access_token=access_token)
-
-        except:
-            return error_message("error in database connection")
+    except:
+        return error_message("error in database connection!")
 
 
-class DoctorLogin(Resource):
+@app.route("/register/user", methods=['PUT'])
+def register_user():
+    if request.method != 'PUT':
+        return make_response("error", 405)
 
-    def get(self):
-        params = request.get_json(silent=True)
-        if params is None:
-            return error_message("wrong parameters")
+    json_data = request.get_json(silent=True, cache=False)
+    if json_data is None:
+        return error_message("mime type not accepted")
 
-        if "cf" not in params or "password" not in params:
-            return error_message("username and password are mandatory fields!")
+    fields = ["cf", "name", "surname", "age", "email", "password", "phone_number"]
 
-        try:
-            user = mongo.db.doctors.find_one({"_id": params["cf"]})
+    for field in fields:
+        if field not in json_data:
+            return error_message(field + " is a mandatory field")
 
-            if user is None:
-                return error_message("user doesn't exists!")
+    try:
+        if mongo.db.users.find_one({'_id': json_data["cf"]}) is not None:
+            return error_message("a user with this CF already exists!")
 
-            pwd = user["password"]
-            if not sha256.verify(params["password"], pwd):
-                return error_message("password is not correct")
+        mongo.db.users.insert_one(
+            {
+                "_id": json_data["cf"],
+                "email": json_data["email"],
+                "password": sha256.hash(json_data["password"]),
+                "name": json_data["name"],
+                "surname": json_data["surname"],
+                "age": json_data["age"],
+                "phone_server": json_data["phone_number"]
+            }
+        )
 
-            access_token = create_access_token({"username": params["cf"], "type": "doctor"})
-            return jsonify(status="ok", access_token=access_token)
+        access_token = create_access_token({"username": json_data["cf"], "type": "user"})
+        return jsonify(status="ok", access_token=access_token)
 
-        except:
-            return error_message("error in database connection!")
-
-
-class DoctorRegister(Resource):
-
-    def put(self):
-        json_data = request.get_json(silent=True, cache=False)
-        if json_data is None:
-            return error_message("mime type not accepted")
-
-        fields = ["cf", "email", "password"]
-
-        for field in fields:
-            if field not in json_data:
-                return error_message(field + " is a mandatory field")
-
-        try:
-            if mongo.db.doctors.find_one({'_id': json_data["cf"]}) is not None:
-                return error_message("a user with this CF already exists!")
-
-            mongo.db.users.insert_one(
-                {
-                    '_id': json_data['cf'],
-                    'email': json_data['email'],
-                    'password': sha256.hash(json_data["password"])
-                }
-            )
-
-            access_token = create_access_token({"username": json_data["cf"], "type": "doctor"})
-            return jsonify(status="ok", access_token=access_token)
-
-        except:
-            return error_message("error in database connection")
+    except:
+        return error_message("error in database connection")
 
 
-api.add_resource(UserLogin, "/login/user")
-api.add_resource(UserRegister, "/register/user")
-api.add_resource(DoctorLogin, "/login/doctor")
-api.add_resource(DoctorRegister, "/register/doctor")
+@app.route("/register/doctor", methods=['PUT'])
+def put():
+    if request.method != "PUT":
+        return make_response("error", 405)
+
+    json_data = request.get_json(silent=True, cache=False)
+    if json_data is None:
+        return error_message("mime type not accepted")
+
+    fields = ["id", "email", "password", "name", "surname", "address"]
+
+    for field in fields:
+        if field not in json_data:
+            return error_message(field + " is a mandatory field")
+
+    try:
+        if mongo.db.doctors.find_one({'_id': json_data["id"]}) is not None:
+            return error_message("a user with this id already exists!")
+
+        mongo.db.users.insert_one(
+            {
+                "_id": json_data["if"],
+                "email": json_data["email"],
+                "password": sha256.hash(json_data["password"]),
+                "name": json_data["name"],
+                "surname": json_data["surname"],
+                "address": json_data["address"],
+                "patients": [],
+                "patient_requests": []
+            }
+        )
+
+        access_token = create_access_token({"username": json_data["cf"], "type": "doctor"})
+        return jsonify(status="ok", access_token=access_token)
+
+    except:
+        return error_message("error in database connection")
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 8080)
