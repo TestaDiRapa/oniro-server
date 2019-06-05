768c68189298410d90be0ffecafcbbb66ad0059a
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-05 11:01:31 +0200

    Added request to doctor

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, make_response
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_claims
 from flask_pymongo import PyMongo
 from passlib.hash import pbkdf2_sha256 as sha256
 from utils import error_message
@@ -51,14 +51,11 @@ def login(user_type):
         return jsonify(status="ok", access_token=access_token)
 
     except:
-        return error_message("error in database connection!")
+        return make_response("error", 500)
 
 
 @app.route("/register/user", methods=['PUT'])
 def register_user():
-    if request.method != 'PUT':
-        return make_response("error", 405)
-
     json_data = request.get_json(silent=True, cache=False)
     if json_data is None:
         return error_message("mime type not accepted")
@@ -89,14 +86,11 @@ def register_user():
         return jsonify(status="ok", access_token=access_token)
 
     except:
-        return error_message("error in database connection")
+        return make_response("error", 500)
 
 
 @app.route("/register/doctor", methods=['PUT'])
 def register_doctor():
-    if request.method != "PUT":
-        return make_response("error", 405)
-
     json_data = request.get_json(silent=True, cache=False)
     if json_data is None:
         return error_message("mime type not accepted")
@@ -128,7 +122,45 @@ def register_doctor():
         return jsonify(status="ok", access_token=access_token)
 
     except:
-        return error_message("error in database connection")
+        return make_response("error", 500)
+
+
+@app.route("/user/subscribe_to_doctor", methods=['POST'])
+@jwt_required
+def subscribe_to_doctor():
+    params = request.get_json(silent=True, cache=False)
+    claims = get_jwt_claims()
+    if claims["type"] != "user":
+        return error_message("only users can subscribe to doctors!")
+
+    if "doctor_id" not in params:
+        return error_message("doctor id is a mandatory parameter")
+
+    try:
+        result = mongo.db.doctors.find_one({'_id': params["doctor_id"]})
+
+        if result is None:
+            return error_message("this doctor doesn't exists!")
+
+        elif claims["username"] in result["patients"]:
+            return error_message("already a patient of this doctor")
+
+        elif claims["username"] in result["patient_requests"]:
+            return error_message("already sent a message to this doctor")
+
+        mongo.db.doctors.update_one(
+            {
+                "_id": params["doctor_id"]
+            },
+            {
+                "$push": {"patient_requests": claims["username"]}
+            }
+        )
+
+        return jsonify(status="ok")
+
+    except:
+        return make_response("error", 500)
 
 
 if __name__ == "__main__":
