535b125f42e4cda519464daca01592a91b41b1a0
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-10 13:14:27 +0200

    Fixed recordings

--- user/my_recordings.py
+++ user/my_recordings.py
@@ -1,11 +1,14 @@
 from flask import jsonify
 from utils import error_message
+from datetime import datetime
 
 
 def my_recordings_put(params, claims, mongo):
 
-    if params is None:
-        return error_message("No payload sent!")
+    mongo.db["record"].insert_one({
+        '_id': datetime.now().timestamp(),
+        'pay': params
+    })
 
     if claims["type"] != "user":
         return error_message("only users can send recordings")
