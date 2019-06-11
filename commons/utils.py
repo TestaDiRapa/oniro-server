84c7180fc8f5450e0c5700799f6bbf72b592d55a
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-11 12:57:46 +0200

    Updated recording processing

--- commons/utils.py
+++ commons/utils.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+from dateutil.parser import parser
 from flask import jsonify
+from numpy import hamming
+from scipy.signal import periodogram
 
 
 def error_message(message):
@@ -6,4 +10,91 @@ def error_message(message):
 
 
 def interval_avg(signal, rate, interval):
-    pass
+
+    if rate >= interval:
+        return signal
+
+    spi = interval//rate
+    approx_len = len(signal) - len(signal) % spi
+
+    i = 0
+    agg_sig = []
+    while i < approx_len:
+        agg_sig.append(signal[i:i+spi])
+        i += spi
+
+    return agg_sig
+
+
+def spectral_analysis(signal, rate):
+
+    ham = hamming(len(signal))
+    ham_wdw = ham*signal
+
+    return periodogram(ham_wdw, 1/rate)
+
+
+def aggregate_apnea_events(oxy_events, dia_events):
+
+    ret = []
+
+    oxy_i = 0
+    dia_i = 0
+
+    while oxy_i < len(oxy_events) and dia_i < len(dia_events):
+        oxy_ev = oxy_events[oxy_i]
+        dia_ev = dia_events[dia_i]
+        o_time = parser().parse(oxy_ev["time"])
+        d_time = parser().parse(oxy_ev["time"])
+
+        if o_time <= d_time:
+            if o_time + timedelta(seconds=oxy_ev["duration"]) < d_time:
+                ret.append({
+                    "time": o_time.strftime("%x %X"),
+                    "duration": oxy_ev["duration"],
+                    "type": "pulseox"
+                })
+
+            else:
+                ret.append({
+                    "time": ((o_time+d_time)/2).strftime("%x %X"),
+                    "duration": (oxy_ev["duration"]+dia_ev["duration"])//2,
+                    "type": "consensus"
+                })
+
+            oxy_i += 1
+
+        else:
+            if d_time + timedelta(seconds=dia_ev["duration"]) < o_time:
+                ret.append({
+                    "time": d_time.strftime("%x %X"),
+                    "duration": dia_ev["duration"],
+                    "type": "diaphragm"
+                })
+
+            else:
+                ret.append({
+                    "time": ((o_time + d_time) / 2).strftime("%x %X"),
+                    "duration": (oxy_ev["duration"] + dia_ev["duration"]) // 2,
+                    "type": "consensus"
+                })
+
+            dia_i += 1
+
+    while dia_i < len(dia_events):
+        ret.append({
+            "time": d_time.strftime("%x %X"),
+            "duration": dia_ev["duration"],
+            "type": "diaphragm"
+        })
+        dia_i += 1
+
+    while oxy_i < len(oxy_events):
+        ret.append({
+            "time": o_time.strftime("%x %X"),
+            "duration": oxy_ev["duration"],
+            "type": "pulseox"
+        })
+        oxy_i += 1
+
+    return ret
--- user/my_recordings.py
+++ user/my_recordings.py
@@ -1,5 +1,54 @@
 from flask import jsonify
-from commons.utils import error_message
+from commons.utils import aggregate_apnea_events, error_message, interval_avg, spectral_analysis
+from statistics import mean
+
+
+def my_recordings_get(identifier, cf, claims, mongo):
+
+    if claims["type"] == "doctor":
+        if cf is None:
+            return error_message("user is a mandatory parameter!")
+
+        try:
+            doc = mongo.db.doctors.find_one({'_id': claims["identity"]})
+
+            if doc is None:
+                return error_message("doctor is not registered!")
+
+            if cf not in doc["patients"]:
+                return error_message("patient not registered!")
+
+            user = cf
+
+        except Exception as e:
+            return error_message(str(e))
+
+    else:
+        user = claims["identity"]
+
+    if identifier is None:
+        try:
+            ret = []
+
+            for document in mongo.db[user].find({"type": "preview"}):
+                ret.append(document)
+
+            return jsonify(status="ok", payload=ret)
+
+        except Exception as e:
+            return error_message(str(e))
+
+    else:
+        try:
+            doc = mongo.db[user].find({"_id": identifier})
+
+            if doc is None:
+                return error_message("recording does not exists!")
+
+            return jsonify(status="ok", payload=doc)
+
+        except Exception as e:
+            return error_message(str(e))
 
 
 def processing(rec_id, claims, mongo):
@@ -12,15 +61,66 @@ def processing(rec_id, claims, mongo):
     if rec_id is None:
         return error_message("id is a mandatory parameter!")
 
+    aggregate = dict()
+    preview = dict()
+
     try:
         record = mongo.db[user].find_one({"_id": rec_id})
 
         if record is None:
             return error_message("no records with the specified id!")
 
-        spo2_rate = 4
+        aggregate["_id"] = rec_id + "A"
+        aggregate["date"] = rec_id
+        aggregate["type"] = "aggregate"
 
+        preview["_id"] = rec_id + "P"
+        preview["date"] = rec_id
+        preview["type"] = "preview"
 
+        aggregate["avg_spo2"] = mean(record["spo2"])
+        aggregate["plot_spo2"] = interval_avg(record["spo2"], record["spo2_rate"], 60)
+
+        preview["avg_spo2"] = aggregate["avg_spo2"]
+
+        aggregate["avg_hr"] = mean(record["hr"])
+        aggregate["plot_hr"] = interval_avg(record["hr"], record["hr_rate"], 60)
+
+        preview["avg_hr"] = aggregate["avg_hr"]
+
+        aggregate["total_movements"] = sum(record["movements_count"])
+        aggregate["plot_movements"] = record["movements_count"]
+
+        f, ps = spectral_analysis(record["spo2"], record["spo2_rate"])
+
+        aggregate["spo2_spectra"] = {
+            "frequencies": f,
+            "spectral_density": ps
+        }
+
+        f, ps = spectral_analysis(record["raw_hr"], record["raw_hr_rate"])
+
+        aggregate["hr_spectra"] = {
+            "frequencies": f,
+            "spectral_density": ps
+        }
+
+        apnea_events = aggregate_apnea_events(record["oxy_events"], record["dia_events"])
+        avg = 0
+        for event in apnea_events:
+            avg += event["duration"]
+        avg = avg // len(apnea_events)
+
+        aggregate["apnea_events"] = len(apnea_events)
+        aggregate["avg_duration"] = avg
+        aggregate["plot_apnea_events"] = apnea_events
+
+        preview["apnea_events"] = len(apnea_events)
+
+        mongo.db[user].insert_one(preview)
+        mongo.db[user].insert_one(aggregate)
+
+        return jsonify(status="ok")
 
     except Exception as e:
         return error_message(str(e))
