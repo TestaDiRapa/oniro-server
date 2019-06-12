950832c6ed103c1ec7ca471f276927d2f24c9c1e
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-12 18:29:53 +0200

    Test

--- server.py
+++ server.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 10
-app.config["JWT_REFRESH_TOKEN_EXPIRES"] = 12000
 
 # Initialize mongo client
 with open("/root/oniro-server/mongourl.secret") as url_file:
