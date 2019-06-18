8cdccf846da3f258737e258609b080b1c2e5d408
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-18 11:59:37 +0200

    Test

--- commons/utils.py
+++ commons/utils.py
@@ -11,8 +11,8 @@ def prepare_packet(record, habit):
     aggregate = dict()
     preview = dict()
 
-    interval = datetime.timedelta(seconds=int(datetime.datetime.now().timestamp() - parser.parse(record["_id"]).timestamp()))
-    
+    interval = int(datetime.datetime.now().timestamp() - parser.parse(record["_id"]).timestamp())
+
     hours = str(interval//3600)
 
     minutes = str((interval - eval(hours)*3600)//60)
